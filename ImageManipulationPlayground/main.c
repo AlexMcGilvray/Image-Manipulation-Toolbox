17f97cff5045b4dcedaf9ff3612e6d6f44a4441b
Alex McGilvray <alex@levelism.com>
2016-05-06 15:07:55 -0700

    main

---
+++ ImageManipulationPlayground/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
+
+
+int main(int argc, char * argv[])
+{
+
+	printf("Hello");
+
+	getchar();
+}
