18686ddd14248a295738c0a9c210ca6ad48427de
Alex McGilvray <alex@levelism.com>
2016-06-30 14:12:27 -0700

    removed the test export functions

--- ImageManipulationPlayground/exports.c
+++ ImageManipulationPlayground/exports.c
@@ -1,16 +1,5 @@
 #include "exports.h"
 
-LIBRARY_API void export_test()
-{
-	printf("EXPORT !! \n");
-}
-
-
-LIBRARY_API void export_test2(const char * someVal)
-{
-	printf("EXPORT %s !! \n", someVal);
-}
-
 //@TODO Determine output format by dest postfix EX image.png -> stbi_write_png 
 LIBRARY_API void export_rotate_image_90_cw(const char * source, const char * dest)
 {
